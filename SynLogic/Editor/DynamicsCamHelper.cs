177cb3033829fd31ee8579cb68cc2ad63d4ecf44
SyntheticLogic <synlogic@users.noreply.github.com>
2022-05-09 17:05:05 -0500

    Added Necessary files.

---
+++ SynLogic/Editor/DynamicsCamHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DynamicsCamHelper : MonoBehaviour
+{
+
+    
+    [MenuItem("GameObject/Set DynamicsCam Focus", false, 0)]
+    static void setFocus()
+    {
+        bool foundHead = false;
+        foreach (DynamicsCam dcam in GameObject.FindObjectsOfType<DynamicsCam>())
+        {
+            Debug.Log("Found DynamicsCam");
+            if (dcam.focusHead)
+            {
+                Debug.Log("Focus head is true!");
+                foreach (Transform t in Selection.activeTransform.GetComponentsInChildren<Transform>())
+                {
+                    if (t.name.ToLower() == "head")
+                    {
+                        Debug.Log("Setting focus");
+                        dcam.SetFocus(t);
+                        foundHead = true;
+                        break;
+                    }
+                }
+            }
+            if (!foundHead) dcam.SetFocus(Selection.activeGameObject.transform);
+        }
+    }
+}
---
+++ SynLogic/Scripts/DynamicsCam.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+    using VRC.Dynamics;
+    using UnityEditor;
+#endif
+
+public class DynamicsCam : MonoBehaviour
+{
+
+    public GameObject senderPrefab;
+    public float minRadiusSize = 0.003f;
+    public Transform focus;
+    public bool focusHead = true;
+    public Vector3 focusOffset = new Vector3(0,0,0);
+    public float moveSpeed = 0.5f;
+    public float shiftSpeedMultiplier = 3f;
+    
+    public float verticalSpeed = 1f;
+    public float rotateSpeed = 3f;
+
+
+    float defaultMoveSpeed;
+    float pitch;
+    float yaw;
+    float radiusSize;
+    float maxRadiusSize = 0f;
+    Transform root;
+    Camera cam;
+
+    GameObject sender;
+    //ContactReceiver[] receivers;
+
+    public void SetFocus(Transform f)
+    {
+        Debug.Log($"Set new focus to {f.name}");
+        this.focus = f;
+        try{
+            Transform parent = focus.parent;
+            while (parent.parent) parent = parent.parent;
+            this.root = parent;
+        } catch(System.Exception e) {}
+    }
+    void Start()
+    {
+        
+        #if UNITY_EDITOR
+
+        if (focusHead) {
+            root = focus;
+            while (root.parent) root = root.parent;
+            foreach (Transform t in root.gameObject.GetComponentsInChildren<Transform>())
+            {
+                if (t.name.ToLower() == "head") focus = t;
+            }
+        }
+
+        //Disable other cameras to keep physbones happy
+        foreach (Camera cam in Camera.allCameras)
+        {
+            if (cam != Camera.main) cam.gameObject.SetActive(false);
+        }
+
+        defaultMoveSpeed = moveSpeed;
+        cam = SceneView.GetAllSceneCameras()[0];
+        this.transform.position = cam.transform.position;
+        this.transform.rotation = cam.transform.rotation;
+        pitch = this.transform.rotation.eulerAngles.x;
+        yaw = this.transform.rotation.eulerAngles.y;
+    
+        ContactReceiver[] receivers = GameObject.FindObjectsOfType<ContactReceiver>();
+        foreach (ContactReceiver receiver in receivers)
+        {
+            Transform parent = receiver.transform.parent;
+            while (parent.parent)
+            {
+                parent = parent.parent;
+            }
+            if (parent.gameObject.activeSelf)
+            {
+                SphereCollider col = receiver.gameObject.AddComponent<SphereCollider>();
+                if (col.radius > maxRadiusSize) maxRadiusSize = col.radius;
+                col.radius = radiusSize = receiver.radius * (0.95f);
+            }
+        }
+        #endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed =  shiftSpeedMultiplier * defaultMoveSpeed;
+        }
+        else moveSpeed = defaultMoveSpeed;
+
+        float hori = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+
+        if (vert != 0)
+        {
+            //transform.localPosition += moveSpeed * new Vector3(-Input.GetAxisRaw("Horizontal"), 0, -Input.GetAxisRaw("Vertical")) * Time.deltaTime;
+            if (vert < 0) transform.Translate(-Vector3.forward * Time.deltaTime * moveSpeed);
+            if (vert > 0) transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        }
+        if (hori != 0)
+        {
+            if (hori < 0) transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+            if (hori > 0) transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        }
+        
+        if (Input.GetMouseButton(1))
+        {
+            yaw += rotateSpeed * 100 * Input.GetAxis("Mouse X") * Time.deltaTime;
+            pitch -= rotateSpeed * 100 * Input.GetAxis("Mouse Y") * Time.deltaTime;
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            Ray ray;
+            RaycastHit hit;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.DrawLine(ray.origin, hit.point, Color.red);
+                
+                if (senderPrefab)
+                {
+                    Debug.Log("Generated Sender Object");
+                    sender = Instantiate(senderPrefab, hit.point, Quaternion.identity);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(2))
+        {
+            GameObject.DestroyImmediate(sender);
+        }
+
+        if (Input.GetKey("q"))
+        {
+            transform.Translate(verticalSpeed * -Vector3.up * Time.deltaTime);
+            //transform.position = new Vector3(transform.position.x, transform.position.y + -verticalSpeed, transform.position.z);
+        }
+
+        if (Input.GetKey("e"))
+        {
+            transform.Translate(verticalSpeed * Vector3.up * Time.deltaTime);
+            //transform.position = new Vector3(transform.position.x, transform.position.y + verticalSpeed, transform.position.z);
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            transform.position = focus.position + focusOffset;
+            transform.LookAt(focus);
+            pitch = transform.eulerAngles.x;
+            yaw = transform.eulerAngles.y;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0 && Input.GetKey(KeyCode.LeftControl))
+        {
+            ContactReceiver[] receivers = GameObject.FindObjectsOfType<ContactReceiver>();
+            radiusSize += 0.01f * Input.GetAxis("Mouse ScrollWheel");
+            radiusSize = Mathf.Clamp(radiusSize, 0f, maxRadiusSize);
+            PlayerPrefs.SetFloat("radiusSize", radiusSize);
+            foreach (ContactReceiver rec in receivers)
+            {
+                SphereCollider col = rec.GetComponent<SphereCollider>();
+                col.radius = Mathf.Clamp(radiusSize, minRadiusSize, rec.radius);
+            }
+        }
+        
+        #if UNITY_EDITOR
+        if (Input.GetKeyDown("f"))
+        {
+            Selection.SetActiveObjectWithContext(root, root);
+        }
+        #endif
+    }
+}
