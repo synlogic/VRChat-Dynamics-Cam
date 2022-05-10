ec89dbba4c4d44bcd5292bb7dba9fc6bf66c2cbd
SyntheticLogic <synlogic@users.noreply.github.com>
2022-05-10 18:41:22 -0500

    Fixed a few bugs, cleaned up UI.

---
+++ SynLogic/Editor/SynTools.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Animations;
+
+public static class SynTools
+{
+    static string[] objectNames = new string[] { "LeftArm", "LeftElbow", "RightArm", "RightElbow"};
+    static string[] twistNames = new string[] {"LeftElbowTwist", "LeftHandTwist", "RightElbowTwist", "RightHandTwist"};
+    static string[] siblingNames = new string[] {"LeftHand", "RightHand", "LeftElbow", "RightElbow"};
+
+    [MenuItem("GameObject/Setup Twistbone <3", false, 0)]
+    static void SetupTwistBone()
+   {
+       try {
+        GameObject root = Selection.activeGameObject;
+            foreach (Transform child in root.GetComponentsInChildren<Transform>())
+            {
+                foreach (string name in objectNames)
+                {
+                    if (name == child.name)
+                    {
+                        foreach (Transform bone in child)
+                        {
+                            foreach (string twistName in twistNames)
+                            {
+                                if (bone.name == twistName)
+                                {
+                                    RotationConstraint rc = Undo.AddComponent<RotationConstraint>(bone.gameObject);
+                                    rc.weight = 0.5f;
+                                    ConstraintSource source = new ConstraintSource();
+                                    foreach (string sibling in siblingNames)
+                                    {
+                                        foreach (Transform siblingBone in child)
+                                        {
+                                            if (sibling == siblingBone.name)
+                                            source.sourceTransform = siblingBone;
+                                        }
+                                    }
+                                    source.weight = 1f;
+                                    rc.AddSource(source);
+                                    rc.rotationAxis = Axis.Y;
+                                    rc.locked = true;
+                                    rc.constraintActive = true;
+                                    
+                                    EditorUtility.SetDirty(bone);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Debug.Log("Setup Twistbones Succesfully (probably)");
+       } 
+       catch (System.Exception e)  {
+           Debug.Log("Something went wrong setting twist bones :(");
+           Debug.LogError(e.ToString());
+       }
+   }
+
+}
+
--- SynLogic/Scripts/DynamicsCam.cs
+++ SynLogic/Scripts/DynamicsCam.cs
@@ -1,24 +1,37 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-#if UNITY_EDITOR
-    using VRC.Dynamics;
-    using UnityEditor;
-#endif
+using VRC.Dynamics;
+using UnityEditor;
+using UnityEngine.SceneManagement;
 
 public class DynamicsCam : MonoBehaviour
 {
-
+    // Help box uses https://github.com/johnearnshaw/unity-inspector-help/
+    [SerializeField]
+    [Help(" * Right click objects in the Hierchy tab to select a focus (Set DynamicsCam Focus)\n\n * Edit the senderPrefab to change collision tags\n\n * Turn on gizmos in game view for a visual representation of collider and contact sizes.")]
+    [Tooltip("The focusable object, right click objects in Hierachy tab to set the focus easily (Set DynamicsCam Focus)")]
     public Transform focus;
-    public bool focusHead = true;
+    [Tooltip("Offset of the camera to the focus object when pressing R")]
     public Vector3 focusOffset = new Vector3(0,0,0.5f);
-    public GameObject senderPrefab;
-    public float minRadiusSize = 0.003f;
-    public float moveSpeed = 0.75f;
-    public float shiftSpeedMultiplier = 3f;
+    [Tooltip("Enabled to focus on the head instead of root of objects when pressing R")]
+    public bool focusHead = true;
     
+    [Header("ContactSender Settings")]
+    [Tooltip("Customizable sender prefab.  Edit this to change contact collision tags.")]
+    public GameObject senderPrefab;
+    [Tooltip("The minimum size the contact receiver will be able to shrink to.")]
+    public float minRadiusSize = 0.003f;
+    
+    [Header("Movement Settings")]
+    [Tooltip("Speed of the vertical camera movement (Q & E)")]
     public float verticalSpeed = 1f;
+    [Tooltip("Speed of the horizontal camera movement (WASD)")]
+    public float moveSpeed = 0.75f;
+    [Tooltip("Multiplayer of movespeed when pressing (Left Shift)")]
+    public float shiftSpeedMultiplier = 3f;
+    [Tooltip("Speed of camera rotation when holding down right click.")]
     public float rotateSpeed = 3;
 
 
@@ -27,32 +40,37 @@ public class DynamicsCam : MonoBehaviour
     float yaw;
     float radiusSize;
     float maxRadiusSize = 0f;
+    bool run = true;
     Transform root;
     Camera cam;
 
     GameObject sender;
-    //ContactReceiver[] receivers;
+
+    Transform GetRoot(Transform p)
+    {
+        if (p.parent) while(p.parent) p = p.parent;
+        return p;
+    }
 
     public void SetFocus(Transform f)
     {
-        Debug.Log($"Set new focus to {f.name}");
+        Debug.Log($"Set new DynamicsCam focus to {f.name}");
         this.focus = f;
-        try{
-            Transform parent = focus.parent;
-            while (parent.parent) parent = parent.parent;
-            this.root = parent;
-        } catch(System.Exception e) {}
+        this.root = GetRoot(focus);
     }
     void Start()
     {
-        
-        #if UNITY_EDITOR
+        // Probably a better way to handle this, will look into it.
+        if (GameObject.Find("VRCSDK"))
+        {
+            this.run = false;
+            return;
+        }
 
         if (!focus) Debug.LogAssertion("DynamicsCam: No focus was set, the script will not work properly.");
 
         if (focusHead) {
-            root = focus;
-            while (root.parent) root = root.parent;
+            root = GetRoot(focus);
             foreach (Transform t in root.gameObject.GetComponentsInChildren<Transform>())
             {
                 if (t.name.ToLower() == "head") focus = t;
@@ -75,27 +93,22 @@ public class DynamicsCam : MonoBehaviour
         ContactReceiver[] receivers = GameObject.FindObjectsOfType<ContactReceiver>();
         foreach (ContactReceiver receiver in receivers)
         {
-            Transform parent = receiver.transform;
-            if (receiver.transform.parent)
-            {
-                while (parent.parent)
-                {
-                    parent = parent.parent;
-                }
-            }
+            Transform parent = GetRoot(receiver.transform);
             if (parent.gameObject.activeSelf)
             {
-                SphereCollider col = receiver.gameObject.AddComponent<SphereCollider>();
+                GameObject colObj = new GameObject("ReceiverCollider");
+                colObj.transform.SetParent(receiver.transform, false);
+                SphereCollider col = colObj.AddComponent<SphereCollider>();
                 if (col.radius > maxRadiusSize) maxRadiusSize = col.radius;
-                col.radius = radiusSize = receiver.radius * (0.95f);
+                col.radius = radiusSize = receiver.radius * (0.1f);
             }
         }
-        #endif
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!run) return;
         if (Input.GetKey(KeyCode.LeftShift))
         {
             moveSpeed =  shiftSpeedMultiplier * defaultMoveSpeed;
@@ -111,6 +124,7 @@ public class DynamicsCam : MonoBehaviour
             if (vert < 0) transform.Translate(-Vector3.forward * Time.deltaTime * moveSpeed);
             if (vert > 0) transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
         }
+
         if (hori != 0)
         {
             if (hori < 0) transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
@@ -133,9 +147,8 @@ public class DynamicsCam : MonoBehaviour
             {
                 Debug.DrawLine(ray.origin, hit.point, Color.red);
                 
-                if (senderPrefab)
+                if (senderPrefab && !hit.transform.GetComponent<ContactReceiver>())
                 {
-                    Debug.Log("Generated Sender Object");
                     sender = Instantiate(senderPrefab, hit.point, Quaternion.identity);
                 }
             }
@@ -171,19 +184,17 @@ public class DynamicsCam : MonoBehaviour
             ContactReceiver[] receivers = GameObject.FindObjectsOfType<ContactReceiver>();
             radiusSize += 0.01f * Input.GetAxis("Mouse ScrollWheel");
             radiusSize = Mathf.Clamp(radiusSize, 0f, maxRadiusSize);
-            PlayerPrefs.SetFloat("radiusSize", radiusSize);
             foreach (ContactReceiver rec in receivers)
-            {
-                SphereCollider col = rec.GetComponent<SphereCollider>();
+            {   
+                SphereCollider col = rec.GetComponentInChildren<SphereCollider>();
                 col.radius = Mathf.Clamp(radiusSize, minRadiusSize, rec.radius);
             }
         }
-        
-        #if UNITY_EDITOR
+
         if (Input.GetKeyDown("f"))
         {
             Selection.SetActiveObjectWithContext(root, root);
         }
-        #endif
     }
 }
+#endif
